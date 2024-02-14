3be5638c2aceeb2e2b37ca5fab89a52c61a985cc
Rafa-Lasso <rlvm13@gmail.com>
2024-02-14 21:55:34 +0100

    modificaciones 1402

--- index.js
+++ index.js
@@ -55,16 +55,94 @@ function ordenarArray(ordenarA) {
     return ordenarA.sort((a, b) => a - b);
 }
 
+const a11=[5,7,3,5,8,4,3,2];
 function obtenerPares(arrayP) {
-    let movimientos = arrayP.lenght();
-    for (i = 0; i < movimientos; i++) {
-        if (arrayP[i] % 2 != 0) {
-            arrayP.shift();
+    let arrayPAux=[];
+    for (let i = 0; i < arrayP.length; i++) {
+        if (arrayP[i] % 2 === 0) {
+            arrayP.push(arrayP[i]);
         }
     }
-    return arrayP;
+    return arrayPAux;
 }
+obtenerPares(a11);
 
+const a12=[0,1,2];
 function pintarArray(array12) {
+console.log(array12);
+let array12Aux=[];
+array12=array12.toString();
+console.log(array12);
+array12Aux=array12.split();
+console.log(array12Aux);
+console.log(array12Aux[0]);
+return array12Aux;
+}
+pintarArray(a12);
+
+function arrayMapi(array13,funcion13){
 
 }
+
+const a14=[3,5,5,2,7];
+function eliminarDuplicados(array14){
+    let arrayAux= new Set(array14);
+    console.log(arrayAux);
+    return arrayAux;
+}
+eliminarDuplicados(a14);
+
+//ITERACIONES PROYECTO
+// Arrays (15-18)
+
+let arrayNumerosNeg=[0,-1,-2.-3,-4,-5,-6,-7,-8,-9];
+const holaMundo=["Hola","Mundo"];
+const loGuardoTodo=["hola","que",23,42.33,"tal"];
+const arrayDeArrays=[[756,"nombre"],[255,"apellido"][298,"direccion"]];
+
+//Funciones (19-22)
+
+function multiplicacion (n1, n2){
+    let resul=n1*n2;
+    return resul;
+}
+multiplicacion(5,5);
+
+function division(n1,n2){
+    let resul=n1/n2;
+    return resul;
+}
+division(6,2);
+
+function esPar(num){
+    if(num%2===0){
+        return true;
+    }
+    else return false;
+}
+esPar(8);
+
+const arrayFunciones=[suma(a,b),resta(a,b),multiplicacion(a,b)];
+
+//Mezclando arrays y funciones (23-26)
+
+const a=[6,3,7,2,9];
+function ordenarArray2(array23){
+    array23.sort((a,b) => a-b);
+    console.log(array23);
+    array23.reverse();
+    console.log(array23);
+    return array23;
+}
+ordenarArray2(a);
+
+const impares=[2,5,4,7,9,3,66];
+function obtenerImpares(array24){
+    for(let i=0;i<array24.lenght;i++){
+        if (array24[i]%2 === 0){
+
+        }
+
+    }
+}
+obtenerImpares(impares);
